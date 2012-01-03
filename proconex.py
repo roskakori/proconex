45ae0df858f628f9de2a6dfb5c36d167a552dfd8
Thomas Aglassinger <roskakori@users.sourceforge.net>
2012-01-03 11:33:56 +0100

    Moved all test related code to test_proconex.

--- proconex.py
+++ proconex.py
@@ -6,23 +6,19 @@ item using command line options.
 from __future__ import with_statement
 
 import logging
-import optparse
 import Queue
 import threading
-import time
 
-_PRODUCTION_DELAY = 0.1
-_CONSUMPTION_DELAY = 0.3
-
-# Delay for ugly hacks and polling loops.
-_HACK_DELAY = 0.05
+# Delay in seconds for ugly polling hacks.
+_HACK_DELAY = 0.02
 
 _log = logging.getLogger("proconex")
+
 
 class WorkEnv(object):
     """
     Environment in which production and consumption takes place and
-    information about a possible error is stored.  
+    information about a possible error is stored.
     """
     def __init__(self):
         self._queue = Queue.Queue()
@@ -60,6 +56,27 @@ class WorkEnv(object):
         return self._error
 
 
+class Producer(object):
+    """
+    Producer putting items on a `WorkEnv`'s queue.
+    """
+    def items(self):
+        """
+        A sequence of items to produce. Typically this is implemented as
+        generator.
+        """
+        raise NotImplementedError()
+
+    def produce(self, workEnv):
+        """
+        Process `items()`. Normally there is no need to modify this procedure.
+        """
+        assert workEnv is not None
+        for item in self.items():
+            workEnv.possiblyRaiseError()
+            workEnv.queue.put(item)
+
+
 class Consumer(threading.Thread):
     """
     Thread to consume items from an item queue filled by a producer, which can
@@ -70,17 +87,16 @@ class Consumer(threading.Thread):
     2. using `cancel()`, which finishes consuming the current item and then
        terminates
     """
-    def __init__(self, name, workEnv):
+    def __init__(self, name):
         assert name is not None
-        assert workEnv is not None
 
         super(Consumer, self).__init__(name=name)
         self._log = logging.getLogger(name)
-        self._workEnv = workEnv
         self.itemToFailAt = None
-        self._log.info(u"waiting for items to consume")
         self._isFinishing = False
         self._isCanceled = False
+        self._workEnv = None
+        self._log.info(u"waiting for items to consume")
 
     def finish(self):
         self._isFinishing = True
@@ -89,12 +105,10 @@ class Consumer(threading.Thread):
         self._isCanceled = True
 
     def consume(self, item):
-        self._log.info(u"consume item %d", item)
-        if item == self.itemToFailAt:
-            raise ValueError("cannot consume item %d" % item)
-        time.sleep(_CONSUMPTION_DELAY)
+        raise NotImplementedError("Consumer.consume()")
 
     def run(self):
+        assert self._workEnv is not None
         try:
             while not (self._isFinishing and self._workEnv.queue.empty()) \
                     and not self._isCanceled:
@@ -115,50 +129,16 @@ class Consumer(threading.Thread):
             self._workEnv.fail(self, error)
 
 
-class Producer(object):
-    def items(self):
-        raise NotImplementedError()
-
-    def produce(self, workEnv):
-        assert workEnv is not None
-        for item in self.items():
-            workEnv.possiblyRaiseError()
-            workEnv.queue.put(item)
-
-
-class SleepyIntegerProducer(Producer):
-    def __init__(self, itemCount, itemProducerFailsAt=None):
-        assert itemCount >= 0
-        assert (itemProducerFailsAt is None) or (itemProducerFailsAt >= 0)
-        self._itemCount = itemCount
-        self._itemProducerFailsAt = itemProducerFailsAt
-        self._log = logging.getLogger("producer")
-
-    def items(self):
-        self._log.info(u"producing %d items", self._itemCount)
-        for item in xrange(self._itemCount):
-            self._log.info(u"produce item %d", item)
-            time.sleep(_PRODUCTION_DELAY)
-            if item == self._itemProducerFailsAt:
-                raise ValueError(u"cannot produce item %d" % item)
-            yield item
-
-
 class Worker(object):
     """
     Controller for interaction between producer and consumers.
     """
-    def __init__(self, itemsToProduceCount, itemProducerFailsAt,
-            itemConsumerFailsAt, consumerCount):
-        # Set up _consumers first so it is available for `__exit__()` even
-        # when `__init__()` fails.
-        self._consumers = None
+    def __init__(self, producer, consumers):
+        assert producer is not None
+        assert consumers is not None
 
-        # Set up the remaining attributes.
-        self._itemsToProduceCount = itemsToProduceCount
-        self._itemProducerFailsAt = itemProducerFailsAt
-        self._itemConsumerFailsAt = itemConsumerFailsAt
-        self._consumerCount = consumerCount
+        self._producer = producer
+        self._consumers = consumers
         self._workEnv = WorkEnv()
 
     def _cancelAllConsumers(self):
@@ -168,8 +148,8 @@ class Worker(object):
                 consumerToCancel.cancel()
             _log.info(u"waiting for consumers to be canceled")
             for possiblyCanceledConsumer in self._consumers:
-                # In this case, we ignore possible consumer errors because there
-                # already is an error to report.
+                # In this case, we ignore possible consumer errors because
+                # there already is an error to report.
                 possiblyCanceledConsumer.join(_HACK_DELAY)
                 if possiblyCanceledConsumer.isAlive():
                     self._consumers.append(possiblyCanceledConsumer)
@@ -178,19 +158,15 @@ class Worker(object):
     def work(self):
         """
         Launch consumer threads and produce items. In case any consumer or the
-        producer raise an exception, fail by raising this exception  
+        producer raise an exception, fail by raising this exception.
         """
-        self._consumers = []
-        for consumerId in range(self._consumerCount):
-            consumerToStart = Consumer(u"consumer %d" % consumerId, self._workEnv)
-            self._consumers.append(consumerToStart)
+        assert self._consumers is not None, "work() must be called only once"
+
+        for consumerToStart in self._consumers:
+            consumerToStart._workEnv = self._workEnv
             consumerToStart.start()
-            if self._itemConsumerFailsAt is not None:
-                consumerToStart.itemToFailAt = self._itemConsumerFailsAt
-        
-        producer = SleepyIntegerProducer(self._itemsToProduceCount, self._itemProducerFailsAt)
-        producer.produce(self._workEnv)
-    
+        self._producer.produce(self._workEnv)
+
         _log.info(u"telling consumers to finish the remaining items")
         for consumerToFinish in self._consumers:
             consumerToFinish.finish()
@@ -206,10 +182,10 @@ class Worker(object):
         """
         Close the whole production and consumption, releasing all resources
         and stopping all threads.
-        
+
         The simplest way to call this is by wrapping the worker in a ``with``
         statement, for example:
-        
+
         >>> with Worker(...) as worker:
         ...    worker.work()
         """
@@ -220,27 +196,3 @@ class Worker(object):
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.close()
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    parser = optparse.OptionParser()
-    parser.add_option("-c", "--consumer-fails-at", metavar="NUMBER",
-        type="long", help="number of items at which consumer fails (default: %default)")
-    parser.add_option("-i", "--items", metavar="NUMBER", type="long",
-        help="number of items to produce (default: %default)", default=10)
-    parser.add_option("-n", "--consumers", metavar="NUMBER", type="long",
-        help="number of consumers (default: %default)", default=2)
-    parser.add_option("-p", "--producer-fails-at", metavar="NUMBER",
-        type="long", help="number of items at which producer fails (default: %default)")
-    options, others = parser.parse_args()
-    worker = Worker(options.items, options.producer_fails_at,
-        options.consumer_fails_at, options.consumers)
-    with worker:
-        try:
-            worker.work()
-            _log.info(u"processed all items")
-        except KeyboardInterrupt:
-            _log.warning(u"interrupted by user")
-        except Exception, error:
-            _log.error(u"%s", error)
--- test/test_proconex.py
+++ test/test_proconex.py
@@ -1,26 +1,84 @@
 """
 Tests for `proconex`.
 """
+from __future__ import with_statement
+
 import logging
+import optparse
+import sys
+import time
 import unittest
 
 import proconex
 
+_PRODUCTION_DELAY = 0.1
+_CONSUMPTION_DELAY = 0.3
+
+_log = logging.getLogger("test_proconex")
+
+
+class SleepyIntegerProducer(proconex.Producer):
+    def __init__(self, itemCount, itemProducerFailsAt=None):
+        assert itemCount >= 0
+        assert (itemProducerFailsAt is None) or (itemProducerFailsAt >= 0)
+        self._itemCount = itemCount
+        self._itemProducerFailsAt = itemProducerFailsAt
+        self._log = logging.getLogger("producer")
+
+    def items(self):
+        self._log.info(u"producing %d items", self._itemCount)
+        for item in xrange(self._itemCount):
+            self._log.info(u"produce item %d", item)
+            time.sleep(_PRODUCTION_DELAY)
+            if item == self._itemProducerFailsAt:
+                raise ValueError(u"cannot produce item %d" % item)
+            yield item
+
+
+class SleepyIntegerConsumer(proconex.Consumer):
+    def __init__(self, name, itemConsumerFailsAt=None):
+        super(SleepyIntegerConsumer, self).__init__(name)
+        self._itemConsumerFailsAt = itemConsumerFailsAt
+
+    def consume(self, item):
+        self._log.info(u"consume item %d", item)
+        if item == self._itemConsumerFailsAt:
+            raise ValueError("cannot consume item %d" % item)
+        time.sleep(_CONSUMPTION_DELAY)
+
+
+def _createWorker(
+        itemCount=10, producerFailsAt=None, consumerFailsAt=None,
+        customerCount=2
+    ):
+    # Create producer and consumers.
+    producer = SleepyIntegerProducer(itemCount, producerFailsAt)
+    consumers = []
+    for consumerId in xrange(customerCount):
+        consumerToStart = SleepyIntegerConsumer(
+            u"consumer %d" % consumerId,
+            consumerFailsAt
+        )
+        consumers.append(consumerToStart)
+
+    return proconex.Worker(producer, consumers)
+
+
 class Test(unittest.TestCase):
     def testCanProduceAndConsume(self):
-        worker = proconex.Worker(10, None,  None, 2)
+        worker = _createWorker(10, None,  None, 2)
         worker.work()
 
     def testCanProduceAndConsumeNothing(self):
-        worker = proconex.Worker(0, None,  None, 2)
+        worker = _createWorker(0, None,  None, 2)
         worker.work()
 
     def testCanProduceLessThanAvailableConsumes(self):
-        worker = proconex.Worker(5, None,  None, 10)
+        worker = _createWorker(5, None,  None, 10)
         worker.work()
 
     def testFailsOnConsumerError(self):
-        worker = proconex.Worker(10, None,  3, 2)
+        worker = _createWorker(10, None,  3, 2)
         try:
             worker.work()
             self.fail("consumer must fail")
@@ -30,7 +88,7 @@ class Test(unittest.TestCase):
             worker.close()
 
     def testFailsOnConsumerErrorAtLastItem(self):
-        worker = proconex.Worker(10, None,  9, 2)
+        worker = _createWorker(10, None,  9, 2)
         try:
             worker.work()
             self.fail("consumer must fail")
@@ -40,7 +98,7 @@ class Test(unittest.TestCase):
             worker.close()
 
     def testFailsOnProducerError(self):
-        worker = proconex.Worker(10, 3, None, 2)
+        worker = _createWorker(10, 3, None, 2)
         try:
             worker.work()
             self.fail("producer must fail")
@@ -48,6 +106,52 @@ class Test(unittest.TestCase):
             self.assertTrue("produce" in unicode(error))
         finally:
             worker.close()
+
+    def testCanRunMainWithDefaults(self):
+        exitCode = main(["test_proconex"])
+        self.assertEquals(exitCode, 0)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    parser = optparse.OptionParser()
+    parser.add_option("-c", "--consumer-fails-at", metavar="NUMBER",
+        type="long",
+        help="number of items at which consumer fails (default: %default)")
+    parser.add_option("-i", "--items", metavar="NUMBER", type="long",
+        help="number of items to produce (default: %default)", default=10)
+    parser.add_option("-n", "--consumers", metavar="NUMBER", type="long",
+        help="number of consumers (default: %default)", default=2)
+    parser.add_option("-p", "--producer-fails-at", metavar="NUMBER",
+        type="long",
+        help="number of items at which producer fails (default: %default)")
+    options, others = parser.parse_args(argv[1:])
+
+    if others:
+        parser.error(u"unknown options must be removed: %s" % others)
+
+    # Create producer and consumers.
+    producer = SleepyIntegerProducer(options.items, options.producer_fails_at)
+    consumers = []
+    for consumerId in xrange(options.consumers):
+        consumerToStart = SleepyIntegerConsumer(
+            u"consumer %d" % consumerId, options.consumer_fails_at
+        )
+        consumers.append(consumerToStart)
+
+    exitCode = 1
+    worker = proconex.Worker(producer, consumers)
+    with worker:
+        try:
+            worker.work()
+            _log.info(u"processed all items")
+            exitCode = 0
+        except KeyboardInterrupt:
+            _log.warning(u"interrupted by user")
+        except Exception, error:
+            _log.error(u"%s", error)
+    return exitCode
 
 
 if __name__ == "__main__":
